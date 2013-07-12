e0edd343fcb28212910dc52df2405df40bdd7e7b
valorzhong <valorz@ZAir.local>
2013-07-12 23:49:44 +0800

    new example02

---
+++ example/example02/example02.dart
@@ -0,0 +1,31 @@
+import 'dart:html';
+import 'package:compass/compass.dart';
+import 'dart:math';
+
+
+void main() {
+  CanvasElement canvas = query('#container');
+  
+  Director.init(canvas, true);
+  director.background = Color.parse(Color.Green);
+  director.replace(new SimpleTest());
+  
+  document.body.children.add(director.stats.container);
+}
+
+class SimpleTest extends Scene {
+  enter() {
+    var rng = new Random();
+    for(var i = 0; i < 10000; i++) {
+      var sprite = new Sprite();
+      sprite.fill = Color.random();
+      sprite.width = rng.nextDouble() * 50;
+      sprite.height = rng.nextDouble() * 50;
+      sprite.x = rng.nextDouble() * director.width;
+      sprite.y = rng.nextDouble() * director.height;
+      sprite.onMouseOver.listen((trigger, e) => trigger.fill = Color.parse(Color.Red));
+      sprite.onMouseOut.listen((trigger, e) => trigger.fill = Color.random());
+      addChild(sprite);
+    }
+  }
+}
