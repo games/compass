b3d1ebf3448c454dc1ff01c172d08bca7cfeb6d6
Valor Zhong <valorzhong@gmail.com>
2013-07-17 14:59:21 +0800

    add new animation

--- example/example03/example03.dart
+++ example/example03/example03.dart
@@ -19,14 +19,24 @@ class SimpleTest extends Scene {
   enter() {
     resources = new ResourceManager();
     resources.addTextureAtlas("atlas", "atlas.json");
+    resources.addTextureAtlas("p3_walk", "p3_walk.json");
     resources.load().then((_) {
       addBird();
+      addWalker();
     });
+  }
+  
+  addWalker() {
+    var atlas = resources.getTextureAtlas("p3_walk");
+    var animate = new SpriteSheet(atlas.getImages("walk_"), 12);
+    animate.x = 100.0;
+    animate.y = 150.0;
+    addChild(animate);
+    director.juggler.add(animate);
   }
   
   addBird() {
     var atlas = resources.getTextureAtlas("atlas");
-    var rng = new Random();
     var animate = new SpriteSheet(atlas.getImages("flight"), 12);
     animate.x = 300.0;
     animate.y = 150.0;
